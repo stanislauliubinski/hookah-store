b2e1e1b5cbd043faa77e60801ba29f51b5f5b5b2
s_liubinski <s.liubinski@naveksoft.com>
2022-04-05 20:22:36 +0300

    initial commit

---
+++ apps/store/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hookah-store-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'store';
+}
